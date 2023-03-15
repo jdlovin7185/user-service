248c4939d36f092aaff0fd4742ca5c503b0aee5f
Joshua Lovin <jdlovin7185@gmail.com>
2023-03-14 21:23:15 -0400

    modifying the websecurityconfig class from the deprecated adapter

--- src/main/java/com/services/user/config/WebConfig.java
+++ src/main/java/com/services/user/config/WebConfig.java
@@ -1,4 +1,11 @@
 package com.services.user.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
 public class WebConfig {
+
+
 }
--- src/main/java/com/services/user/config/WebSecurityConfig.java
+++ src/main/java/com/services/user/config/WebSecurityConfig.java
@@ -2,22 +2,50 @@ package com.services.user.config;
 
 import com.services.user.Service.UserService;
 import lombok.AllArgsConstructor;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @AllArgsConstructor
-public class WebSecurityConfig extends WebSecurityConfiguration {
+public class WebSecurityConfig {
 
     private final UserService userService;
 
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/sign-up/", "/sign-in")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/sign-in")
+                .permitAll();
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        return userService.confirmUser();
+    }
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userService)
+//                .passwordEncoder(bCryptPasswordEncoder);
+//    }
 }
