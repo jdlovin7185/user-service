6227b2d757a616a5aade4e8655b9c8ac6d4cbfa9
Josh Lovin <jdlovin7185@gmail.com>
2023-03-18 14:51:14 -0400

    figuring out life with spring secuirty

--- src/main/java/com/services/user/config/WebSecurityConfig.java
+++ src/main/java/com/services/user/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,26 +18,27 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @AllArgsConstructor
-public class WebSecurityConfig implements WebSecurityConfigurer<WebSecurity> {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserService userService;
 
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring()
-                .anyRequest("/css/**", "/js/**", "/images/**", "/", "/home", "/about")
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/sign-up/**", "/sign-in/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/sign-in")
+                .permitAll();
 
     }
 
-    @Override
-    public void init(WebSecurity builder) throws Exception {
-
-    }
-
-    @Override
-    public void configure(WebSecurity builder) throws Exception {
-
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+                .passwordEncoder(bCryptPasswordEncoder);
     }
 }
