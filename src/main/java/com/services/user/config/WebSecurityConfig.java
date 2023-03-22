521581ac99c69d7c67af43890b14289368b3c41e
Josh Lovin <jdlovin7185@gmail.com>
2023-03-21 21:29:33 -0400

    changed dependecies and called the right class for WebSecurityConfig

--- src/main/java/com/services/user/config/WebSecurityConfig.java
+++ src/main/java/com/services/user/config/WebSecurityConfig.java
@@ -1,32 +1,25 @@
 package com.services.user.config;
 
-import com.services.user.Service.UserService;
+import com.services.user.Service.UserServiceImpl;
 import lombok.AllArgsConstructor;
-
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @AllArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserService userService;
+    private final UserServiceImpl userService;
 
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
+    @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .requestMatchers("/sign-up/**", "/sign-in/**")
+        http.authorizeRequests()
+                .antMatchers("/sign-up/**", "/sign-in/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
@@ -34,9 +27,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/sign-in")
                 .permitAll();
-
     }
 
+    @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService)
                 .passwordEncoder(bCryptPasswordEncoder);
