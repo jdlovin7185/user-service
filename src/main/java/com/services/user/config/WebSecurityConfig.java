6311a2999e251af4912c22ade0f429b559f64bb3
Joshua Lovin <jdlovin7185@gmail.com>
2023-03-28 19:15:18 -0400

    enabled web security annotation works wonders

--- src/main/java/com/services/user/config/WebSecurityConfig.java
+++ src/main/java/com/services/user/config/WebSecurityConfig.java
@@ -2,34 +2,29 @@ package com.services.user.config;
 
 import com.services.user.Service.UserServiceImpl;
 import lombok.AllArgsConstructor;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 @AllArgsConstructor
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
 
     private final UserServiceImpl userService;
 
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .requestMatchers("/sign-up/", "/sign-in")
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/sign-in/**", "/sign-up/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
@@ -37,17 +32,41 @@ public class WebSecurityConfig {
                 .formLogin()
                 .loginPage("/sign-in")
                 .permitAll();
-        return http.build();
     }
-
-    @Bean
-   public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
-
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService)
                 .passwordEncoder(bCryptPasswordEncoder);
     }
+
+
+
+
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests()
+//                .requestMatchers("/sign-in")
+//                .permitAll()
+//                .anyRequest()
+//
+////                .permitAll()
+////                .anyRequest()
+////                .authenticated()
+////                .and()
+////                .formLogin()
+////                .loginPage("/sign-in")
+////                .permitAll();
+//        return http.build();
+//    }
+
+//    @Bean
+//   public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+//        return authenticationConfiguration.getAuthenticationManager();
+//    }
+
+
 }
